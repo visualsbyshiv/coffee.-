0155e0d3a603063c5bbce0d8b45c39799cc30cc4
visualsbyshiv <visualsbyshiv49@gmail.com>
2025-06-09 18:18:22 +0530

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,106 @@
+// for(a=2; a<=20; a++){
+//     console.log(a)
+// }
+// let b=2
+// while(b<=13){
+//     console.log(b); b++;
+// }
+// let c=5
+// while(c>=1){
+//     console.log(c);c--;
+// }
+// let g=1
+// while(g<=20){
+//     console.log(g);g++;
+// }
+// let h=2
+// while (h<=8){
+//     console.log(h);h++
+// }
+
+// let j=1
+// do{
+//     console.log(j);j++;
+// }
+// while(j<=14)
+
+//     let fivechild=["gagu","geeta","gaytonda","ganesh","guru"];
+//     console.log(fivechild[4]);
+
+//     let age=[20,23,30,25,40];
+//     console.log(age[3]);
+//     if (age<=23){
+//         console.log(pass)
+//     }
+//     else if(age>=20){
+//         console.log(fail)
+//     }
+    
+
+//     let star=5
+//     for (let i=0 ; i<star; i++){
+//         let row=" ";
+//     for (let j=0 ; j<i; j++)
+//         row+="#";
+//     console.log(row)
+//     }
+    
+    // dom----------------------------------
+// element by id--------------------------------------------
+// let hed= document.getElementById("hello");{
+//     console.log(hed.style.color="red")
+// }
+
+// elementbydiv_----------------------------------
+
+// function changetext(){
+//     let fronte =document.getElementsByClassName("info");
+//      fronte[0].textContent="hello its shivam";
+//      fronte[1].style.color="blue";
+
+//      for(i=0; i<fronte.length; i++);{
+//         fronte[i].style.backgroundcolor="red"
+//      }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple", "banana", "mango"];
+// console.log(fruits[1+1]); 
